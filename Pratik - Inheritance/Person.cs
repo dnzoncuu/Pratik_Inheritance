cc08cae6881247c718228f6878d6231248f0f337
dnzoncuu <berkoncu17@gmail.com>
2025-05-25 13:36:15 +0300

    Add project files.

---
+++ Pratik - Inheritance/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik___Inheritance
+{
+    public abstract class Person
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+
+        public void AdSoyadYazdir()
+        {
+            Console.WriteLine($"Ad: {Ad}, Soyad: {Soyad}");
+        }
+    }
+
+    public class Ogrenci:Person
+    {
+        public int OgrenciNo {get; set;}
+
+        public void BilgileriYazdir()
+        {
+            AdSoyadYazdir();
+            Console.WriteLine($" Ogrenci Numarası : {OgrenciNo}");
+        }
+    }
+
+    public class Ogretmen : Person
+    {
+        public int Maas { get; set;}
+
+        public void BilgileriYazdir()
+        {
+            AdSoyadYazdir();
+            Console.WriteLine($" Ogretmen maaşı : {Maas}");
+        }
+    }
+}
---
+++ Pratik - Inheritance/Program.cs
@@ -0,0 +1,17 @@
+﻿using Pratik___Inheritance;
+
+Ogrenci ogr = new Ogrenci
+{
+    Ad = "Deniz",
+    Soyad = "Öncü",
+    OgrenciNo = 15647
+};
+ogr.BilgileriYazdir();
+
+Ogretmen ogretmen = new Ogretmen
+{
+    Ad = "cemal",
+    Soyad = "Çelik",
+    Maas = 1234
+};
+ogretmen.BilgileriYazdir();
